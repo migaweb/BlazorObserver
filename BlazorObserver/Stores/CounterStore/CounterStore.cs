10fdd37a083a028c4f1b29f166839a0231ac8b9d
Mikael Storman Gustavsson <miga02@gmail.com>
2021-06-04 19:02:01 +0200

    Add project files.

---
+++ BlazorObserver/Stores/CounterStore/CounterStore.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorObserver.Stores.CounterStore
+{
+  public class CounterState
+  {
+    public CounterState(int count)
+    {
+      Count = count;
+    }
+    public int Count { get; }
+  }
+
+  public class CounterStore
+  {
+    private CounterState _counterState;
+    private Action _listeners;
+
+    public CounterStore()
+    {
+      _counterState = new CounterState(0);
+    }
+
+    public CounterState GetState()
+    {
+      return _counterState;
+    }
+
+    public void IncrementCount()
+    {
+      var count = this._counterState.Count;
+      count++;
+      this._counterState = new CounterState(count);
+      BroadcastStateChanged();
+    }
+
+    public void DecrementCount()
+    {
+      var count = this._counterState.Count;
+      count--;
+      this._counterState = new CounterState(count);
+      BroadcastStateChanged();
+    }
+
+    public void AddStateChangeListeners(Action listener)
+    {
+      _listeners += listener;
+    }
+
+    public void RemoveStateChangeListeners(Action listener)
+    {
+      _listeners -= listener;
+    }
+
+    private void BroadcastStateChanged()
+    {
+      _listeners.Invoke();
+    }
+  }
+}
